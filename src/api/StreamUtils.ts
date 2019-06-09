6a402f4d1f9d5341fb50fdd2716d7e5d6ec1c46d
Qi Liang <liangqi@cn.ibm.com>
2019-06-09 08:33:06 +0800

    Added streamToStream() and use it in download
    Signed-off-by: Qi Liang <liangqi@cn.ibm.com>

--- src/api/StreamUtils.ts
+++ src/api/StreamUtils.ts
@@ -10,7 +10,7 @@
  */
 
 import { IHandlerResponseApi, ITaskWithStatus, TaskStage, TextUtils } from "@brightside/imperative";
-import { Readable } from "stream";
+import { Readable, Writable } from "stream";
 
 export class StreamUtils {
 
@@ -46,6 +46,47 @@ export class StreamUtils {
                     response.progress.endBar();
                     reject(error);
                 });
+            }).catch((streamRejection: any) => {
+                reject(streamRejection);
+            });
+        });
+    }
+
+    /**
+     * Pipes the readable stream to the writable stream.
+     *
+     * @param  stremPromise - promise that will resolve to a readable stream
+     * @param  writable - writable stream
+     * @param  response - response object from your handler, provide if
+     *                                         you want a progress bar created
+     * @returns  - promise that resolves when the h
+     */
+    public static async streamToStream(streamPromise: Promise<Readable>, writable: Writable, response?: IHandlerResponseApi): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            let downloadedBytes = 0;
+            const statusMessage = "Downloaded %d of ? bytes";
+            const task: ITaskWithStatus = {
+                statusMessage: "Starting transfer...",
+                percentComplete: 0,
+                stageName: TaskStage.IN_PROGRESS
+            };
+            if (response != null) {
+                response.progress.startBar({task});
+            }
+            streamPromise.then((stream) => {
+                stream.on("data", (chunk: Buffer) => {
+                    downloadedBytes += chunk.length;
+                    task.statusMessage = TextUtils.formatMessage(statusMessage, downloadedBytes);
+                });
+                stream.on("end", () => {
+                    response.progress.endBar();
+                    resolve("end");
+                });
+                stream.on("error", (error: any) => {
+                    response.progress.endBar();
+                    reject(error);
+                });
+                stream.pipe(writable);
             }).catch((streamRejection: any) => {
                 reject(streamRejection);
             });
