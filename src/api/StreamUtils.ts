99aaebd9c8f6efd4d4e254252e4267439f72de0a
Qi Liang <liangqi@cn.ibm.com>
2019-09-24 18:15:31 +0800

    Fix merge error

--- src/api/StreamUtils.ts
+++ src/api/StreamUtils.ts
@@ -12,6 +12,8 @@
 import { IHandlerResponseApi, ITaskWithStatus, TaskStage, TextUtils } from "@brightside/imperative";
 import { Readable, Writable } from "stream";
 
+const PERCETAGE = 100;
+
 export class StreamUtils {
 
     /**
@@ -22,7 +24,6 @@ export class StreamUtils {
      * @returns  - promise that resolves when the h
      */
     public static async streamToBuffer(estimatedSize: number, streamPromise: Promise<Readable>, response?: IHandlerResponseApi): Promise<Buffer> {
-        const PERCETAGE = 100;
         return new Promise<Buffer>((resolve, reject) => {
             let data: Buffer = Buffer.from([]);
             const statusMessage = "Downloaded %d of %d (Estimated) bytes";
@@ -57,16 +58,18 @@ export class StreamUtils {
     /**
      * Pipes the readable stream to the writable stream.
      *
+     * @param  estimatedSize - estimated file size
      * @param  stremPromise - promise that will resolve to a readable stream
      * @param  writable - writable stream
      * @param  response - response object from your handler, provide if
      *                                         you want a progress bar created
      * @returns  - promise that resolves when the h
      */
-    public static async streamToStream(streamPromise: Promise<Readable>, writable: Writable, response?: IHandlerResponseApi): Promise<string> {
+    public static async streamToStream(estimatedSize: number, streamPromise: Promise<Readable>,
+                                       writable: Writable, response?: IHandlerResponseApi): Promise<string> {
         return new Promise<string>((resolve, reject) => {
             let downloadedBytes = 0;
-            const statusMessage = "Downloaded %d of ? bytes";
+            const statusMessage = "Downloaded %d of %d (Estimated) bytes";
             const task: ITaskWithStatus = {
                 statusMessage: "Starting transfer...",
                 percentComplete: 0,
@@ -78,7 +81,8 @@ export class StreamUtils {
             streamPromise.then((stream) => {
                 stream.on("data", (chunk: Buffer) => {
                     downloadedBytes += chunk.length;
-                    task.statusMessage = TextUtils.formatMessage(statusMessage, downloadedBytes);
+                    task.percentComplete = PERCETAGE * downloadedBytes / estimatedSize;
+                    task.statusMessage = TextUtils.formatMessage(statusMessage, downloadedBytes, estimatedSize);
                 });
                 stream.on("end", () => {
                     response.progress.endBar();
