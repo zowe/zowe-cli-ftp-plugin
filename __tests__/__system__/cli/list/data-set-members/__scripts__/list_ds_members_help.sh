e60fffef404853dee2164b3b615128f26dd645c9
tian na <tiantn@cn.ibm.com>
2020-05-07 21:37:18 +0800

    add list members

---
+++ __tests__/__system__/cli/list/data-set-members/__scripts__/list_ds_members_help.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "================z/OS FTP LIST DATA-SET HELP==============="
+bright zos-ftp list data-set-members --help
+
+echo "================z/OS FTP LIST DATA-SET HELP WITH RFJ==========="
+bright zos-ftp list data-set-members --help --rfj
