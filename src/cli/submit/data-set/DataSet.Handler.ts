4bde92a0846f3f2c828c7abbad0e36e3488ef208
tian na <tiantn@cn.ibm.com>
2020-06-03 17:35:07 +0800

    submit job and get rc v1
    Signed-off-by: tian na <tiantn@cn.ibm.com>

--- src/cli/submit/data-set/DataSet.Handler.ts
+++ src/cli/submit/data-set/DataSet.Handler.ts
@@ -24,37 +24,54 @@ export default class SubmitJobFromLocalFileHandler extends FTPBaseHandler {
         jobid = await params.connection.submitJCL(dsContent);
         if (params.arguments.wait) {
             return new Promise((resolve, reject) => {
-                const interval = 100;
+                const interval = 5000;
+                let num = 0;
                 const timerId = setInterval(async () => {
-                    // jobDetails = await JobUtils.findJobByID(jobid, params.connection);
-                    // tslint:disable-next-line: no-shadowed-variable
-                    jobDetails = await JobUtils.findJobByID(jobid, params.connection);
-                    const status = jobDetails.status.toString();
-                    const successMsg = params.response.console.log(status);
-                    this.log.info(successMsg);
-                    // resolve();
-                    if (status === "OUTPUT") {
-                        setTimeout(() => { clearInterval(timerId); }, 0);
-                        // tslint:disable-next-line: max-line-length no-shadowed-variable
-                        const successMsg = params.response.console.log("Submitted job successfully!", jobDetails.jobname, jobDetails.jobid);
-                        this.log.info(successMsg);
-                        params.response.data.setObj(jobDetails);
-                        params.response.format.output({
-                            output: jobDetails,
-                            format: "object",
-                            fields: ["jobid", "jobname", "owner", "status", "rc"]
-                        });
-                    } else {
-                        // tslint:disable-next-line: max-line-length no-shadowed-variable
-                        const successMsg = params.response.console.log("Job is runing, palease wait!", jobDetails.jobname, jobDetails.jobid);
-                        this.log.info(successMsg);
+                    try {
+                        jobDetails = await JobUtils.findJobByID(jobid, params.connection);
+                        const status = jobDetails.status.toString();
+                        if (status === "OUTPUT") {
+                            // tslint:disable-next-line: no-shadowed-variable
+                            const successMsg = params.response.console.log("Submitted job successfully!", jobDetails.jobname, jobDetails.jobid);
+                            this.log.info(successMsg);
+                            params.response.data.setObj(jobDetails);
+                            params.response.format.output({
+                                output: jobDetails,
+                                format: "object",
+                                fields: ["jobid", "jobname", "owner", "status", "rc"]
+                            });
+                            setTimeout(() => {
+                                clearInterval(timerId);
+                                resolve();
+                            }, 0);
+                            // tslint:disable-next-line: no-magic-numbers
+                        } else if (num === 10) {
+                            // tslint:disable-next-line: max-line-length
+                            const stopcheckMsg = params.response.console.log("Job is runing, palease check status later!", jobDetails.jobname, jobDetails.jobid);
+                            this.log.info(stopcheckMsg);
+                            setTimeout(() => {
+                                clearInterval(timerId);
+                                resolve();
+                            }, 0);
+                        }
+                        else {
+                            const waitMsg = params.response.console.log("Job is runing, palease wait!", jobDetails.jobname, jobDetails.jobid);
+                            this.log.info(waitMsg);
+                            num = num + 1;
+                        }
+                    }
+                    catch (err) {
+                        const errMsg = params.response.console.log(err);
+                        this.log.info(errMsg);
+                        reject();
                     }
                 }, interval);
             });
         }
         else {
             jobDetails = await JobUtils.findJobByID(jobid, params.connection);
-            this.log.info("Submitted job successfully!", jobDetails.jobname, jobDetails.jobid);
+            const successMsg = params.response.console.log("Submitted job successfully!", jobDetails.jobname, jobDetails.jobid);
+            this.log.info(successMsg);
             params.response.data.setObj(jobDetails);
             params.response.format.output({
                 output: jobDetails,
