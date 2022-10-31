9adbc192077d04a3b8370314da23c7d8b23d5f39
Timothy Johnson <timothy.johnson@broadcom.com>
2022-10-31 10:06:47 -0400

    Release with GitHub Actions instead of Jenkins
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ release.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    branches: [
+        {
+            name: "master",
+            level: "minor",
+            devDependencies: ["@zowe/cli", "@zowe/imperative", "@zowe/cli-test-utils"]
+        },
+        {
+            name: "zowe-v1-lts",
+            level: "patch",
+            devDependencies: ["@zowe/cli", "@zowe/imperative"]
+        }
+        // {
+        //     name: "next",
+        //     prerelease: true,
+        //     devDependencies: ["@zowe/cli", "@zowe/imperative", "@zowe/cli-test-utils"]
+        // }
+    ],
+    plugins: [
+        "@octorelease/changelog",
+        ["@octorelease/npm", {
+            aliasTags: {
+                latest: ["zowe-v2-lts", "next"]
+                // latest: ["zowe-v2-lts"]
+            },
+            pruneShrinkwrap: true
+        }],
+        ["@octorelease/github", {
+            checkPrLabels: true
+        }],
+        "@octorelease/git"
+    ]
+};
